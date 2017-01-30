93cc2619c4ecf25f395102d0937c0efff59af1d5
Yulin <yliu677@wisc.edu>
2017-01-30 14:26:14 -0600

    Add files via upload

--- HW1.R
+++ HW1.R
@@ -5,13 +5,19 @@ setwd("~/Desktop/assignment/2016-1017 spring/479/Hw1")
 Wisconsin<-read.csv("WI16.txt",header = TRUE,sep=",")
 
 #pick some columns I am interested in.
+State.ID<-Wisconsin[,1]
+
 structure_number<-Wisconsin[,2]
 
 route_number<-Wisconsin[,6]
+
+county.number<-Wisconsin[,9]
 
 owner<-Wisconsin[,25]
 
 year_built<-Wisconsin[,27]
 
 #create one new data frame
-Wis<-data.frame(structure_number,route_number,owner,year_built)
+Wis<-data.frame(State.ID,structure_number,
+                route_number,county.number,
+                owner,year_built)
